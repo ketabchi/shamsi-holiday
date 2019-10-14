f7da8762324cc2f59183b77e34a748e56f93657e
Mohammad Zolfaghari <zidom72@gmail.com>
2019-10-14 12:32:20 +0330

    npm package

---
+++ index.js
@@ -0,0 +1,11 @@
+const holidays = require('./holidays.json')
+
+function isHoliday(date) {
+	let y = date.getFullYear(), m = date.getMonth()+1, d = date.getDate();
+	d = ('0' + d).slice(-2);
+	m = ('0' + m).slice(-2);
+
+	return holidays.includes(`${y}/${m}/${d}`);
+}
+
+module.exports = isHoliday
