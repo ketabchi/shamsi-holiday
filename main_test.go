a5109cf301ecf46573355d40dd6e489377c55634
Mohammad Zolfaghari <zidom72@gmail.com>
2021-03-07 14:32:16 +0330

    pass tests

--- main_test.go
+++ main_test.go
@@ -2,51 +2,13 @@ package main
 
 import "testing"
 
-func TestIsFriday(t *testing.T) {
-	tests := []struct {
-		m, d int
-		exp  bool
-	}{
-		{7, 26, true},
-		{7, 3, false},
-		{12, 29, false},
-		{1, 2, true},
-	}
+func TestToDateFormat(t *testing.T) {
+	j, m := toDateFormat(12, 29)
 
-	for i, test := range tests {
-		if res := isFriday(test.m, test.d); res != test.exp {
-			t.Errorf("Test %d: Expected isFriday return %t, but got %t",
-				i, test.exp, res)
-		}
+	if j != "1399/12/29" {
+		t.Errorf("Expected 1398/12/29 got %s", j)
 	}
-}
-
-func TestIsShamsiHoliday(t *testing.T) {
-	tests := []struct {
-		m, d int
-		exp  bool
-	}{
-		{8, 5, true},
-		{7, 3, false},
-		{12, 29, true},
-		{1, 1, true},
-	}
-
-	for i, test := range tests {
-		if res := isShamsiHoliday(test.m, test.d); res != test.exp {
-			t.Errorf("Test %d: Expected isShamsiHoliday return %t, but got %t",
-				i, test.exp, res)
-		}
-	}
-}
-
-func TestAddHoliday(t *testing.T) {
-	addHoliday(12, 29)
-
-	if holidays[0] != "1398/12/29" {
-		t.Errorf("Expected 1398/12/29 in holidaays %s", holidays)
-	}
-	if holidays[1] != "2020/03/19" {
-		t.Errorf("Expected 2020/03/19 in holidaays %s", holidays)
+	if m != "2021/03/19" {
+		t.Errorf("Expected 2020/03/19 got %s", m)
 	}
 }
